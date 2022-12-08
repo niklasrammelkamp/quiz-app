2f0dfdf73ee4a4ba12fbc0414ab9febf5a0b922a
niklasrammelkamp <niklas.rammelkammp@gmail.com>
2022-12-08 18:16:26 +0100

    bookmark feature finish

--- js/buttons.js
+++ js/buttons.js
@@ -1,25 +1,22 @@
 console.clear();
 
-const buttons = document.querySelectorAll('[class="show-content"]');
+export function giveShowHideButtonsEventListener(buttons) {
+  for (const element of buttons) {
+    element.addEventListener("click", (event) => {
+      const answer = event.path[2].querySelector('[data-js="answer"]');
 
-for (const element of buttons) {
-  element.addEventListener("click", (event) => {
-    const answer = event.path[2].querySelector('[data-js="answer"]');
+      answer.classList.toggle("hiddenAnswer");
 
-    answer.classList.toggle("hiddenAnswer");
-    console.log(element.textContent.includes("Hide answer"));
-
-    if (element.textContent.includes("Show answer")) {
-      console.log("hello");
-      console.log(element.innerHTML);
-      element.textContent = "Hide answer";
-      return;
-    }
-    if (element.textContent.includes("Hide answer")) {
-      element.textContent = "Show answer";
-      return;
-    }
-  });
+      if (element.textContent.includes("Show answer")) {
+        element.textContent = "Hide answer";
+        return;
+      }
+      if (element.textContent.includes("Hide answer")) {
+        element.textContent = "Show answer";
+        return;
+      }
+    });
+  }
 }
 
 //____________________BOOKMARK BUTTONS____________________
@@ -30,13 +27,11 @@ for (const element of bookmarkButtons) {
   element.addEventListener("click", () => {
     const svg = element.getElementsByTagName("svg")[0];
 
-    console.log(svg.style.fill);
     if (svg.style.fill.includes("var(--primary-color)")) {
       svg.style.fill = "";
       return;
     }
     if (svg.style.fill.includes("")) {
-      console.log("hi");
       svg.style.fill = "var(--primary-color)";
       return;
     }
